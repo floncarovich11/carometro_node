0ed1b5b92092a2dc2dc11688120989ac914c4163
floncarovich11 <felipeloncarovichdossantos@gmail.com>
2024-05-29 17:10:09 -0300

    fdss

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrytp = require('bcrypt');
+require('dotenv').config();
+
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect(err => {
+    if(err){
+        console.error(
+            "Erro ao conectar com banco de dados", err
+        )
+        return; 
+    }
+    console.log('conectado com o bando de dados!')
+});
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUnitialized: true,
+    cookie: { secure: false}
+}));
+
+const authenticateSession = (req, res, next) => {
+    if(!req.session.usedId) {
+        return res.status(401).send(
+            'Acesso negado, faça login para continuar')
+    }
+    next();
+}
+
+app.post('/login', (req, res) => {
+
+    const {cpf, senha} = req.body;
+
+    db.query('SELECT * FROM usuarios WHERE cpf = ?', [cpf],
+    async (err, results) => {
+        if(err) return res.status(500)._construct.send('Server com erro');
+        if(results.lenght == 0) return res.status(500).send('CPF ou senha incorretas')
+
+        const usuario = results[0];
+        const senhaCorreta = await bcrytp.compare(senha, usuario.senha);
+        if(!senhaCorreta) return res.status(500).send(
+            'CPF ou senha incorretos');
+
+        req.session.useId = usuario.idUsuarios;
+        console.log('idUsuario')
+        res.json({message: 'Login bem-sucessido'});
+    });
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
